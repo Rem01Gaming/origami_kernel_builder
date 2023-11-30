c73073b63acba370e2d33925e5c91bc53feb59c4
Rem01Gaming <Rem01_Gaming@proton.me>
2023-11-30 08:37:44 +0700

    Uprev to v1.1
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- origami_kernel_builder.sh
+++ origami_kernel_builder.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Origami Kernel Builder
-# Version 1.0
+# Version 1.1
 # Copyright (c) 2023-2024 Rem01 Gaming <Rem01_Gaming@proton.me>
 #
 #			GNU GENERAL PUBLIC LICENSE
@@ -12,13 +12,12 @@
 
 # Define some things
 # Kernel common
-ARCH=arm64
-DEFCONFIG=even_defconfig
-localversion=-X1.6
-LINKER="ld.lld"
-USE_LLVM=0
+export ARCH=arm64
+export DEFCONFIG=even_defconfig
+export localversion=-X1.6
+export LINKER="ld.lld"
 # Telegram API
-SEND_TO_TG=1
+export SEND_TO_TG=1
 export chat_id=""
 export token=""
 # Telegram && Output
@@ -27,85 +26,79 @@ export CODENAME="even"
 export DEVICE="Realme C25 and Narzo50A (${CODENAME})"
 export BUILDER="Rem01"
 export BUILD_HOST="DigitalOcean"
-TIMESTAMP=$(date +"%Y%m%d")
-export TIMESTAMP
-KBUILD_COMPILER_STRING=$(./clang/bin/clang -v 2>&1 | head -n 1 | sed 's/(https..*//' | sed 's/ version//')
-export KBUILD_COMPILER_STRING
+export TIMESTAMP=$(date +"%Y%m%d")-$(date +"%H%M%S")
+export KBUILD_COMPILER_STRING=$(./clang/bin/clang -v 2>&1 | head -n 1 | sed 's/(https..*//' | sed 's/ version//')
 export FW="RUI2"
-export random_num=${RANDOM}
-export zipn="Liquid-${CODENAME}-${FW}-${TIMESTAMP}-${random_num}"
+export zipn="Liquid-${CODENAME}-${FW}-${TIMESTAMP}"
 # Needed by script
+export PATH="${PWD}/clang/bin:${PATH}"
 PROCS=$(nproc --all)
+
+# Text coloring
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
 
 # Check permission
 script_permissions=$(stat -c %a "$0")
-
 if [ "$script_permissions" -lt 777 ]; then
-    echo "error: don't have enough permission!"
+    echo -e "${RED}error:${NOCOLOR} Don't have enough permission"
     echo "run 'chmod 0777 origami_kernel_builder.sh' and rerun"
     exit 126
 fi
 
 # Check requirements
 if ! hash make curl bc 2>/dev/null; then
+        echo -e "${RED}error:${NOCOLOR} Environment has missing dependencies"
         echo "Install make, curl, and bc !"
         exit 127
-    fi
+fi
 
-# Check the compiller and anykernel
 if [ ! -d "${PWD}/clang" ]; then
-       echo "/clang not found!"
-       echo "have you clone the clang?"
-       exit 2
-    fi
+    echo "/clang not found!"
+    echo "have you clone the clang?"
+    exit 2
+fi
 
-if [ ! -d "${PWD}/arm-gcc" ]; then
-       echo "/arm-gcc not found!"
-       echo "have you clone the arm-gcc?"
-       exit 2
-    fi
 
-if [ ! -d "${PWD}/aarch64-gcc" ]; then
-       echo "/aarch64-gcc not found!"
-       echo "have you clone the aarch64-gcc?"
-       exit 2
-    fi
+if [ ! -d "${PWD}/anykernel" ]; then
+    echo "/anykernel not found!"
+    echo "have you clone the anykernel?"
+    exit 2
+fi
 
-if [ ! -d "${PWD}/anykernel3" ]; then
-       echo "/anykernel3 not found!"
-       echo "have you clone the anykernel3?"
-       exit 2
-    fi
-
-# A function to exit on SIGINT and INT TERM.
-exit_on_signal_INTR() {
-    echo "Got interrupt signal, Aborting..."
-    make clean && make mrproper
-    if [ "$SEND_TO_TG" -eq 1 ]; then
-            curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-            -d chat_id="$chat_id" \
-            -d "disable_web_page_preview=true" \
-            -d "parse_mode=html" \
-            -d text="Got interrupt signal, exited with error status 130."
-        fi
-    exit 130
+help_msg() {
+    echo "Usage: bash origami_kernel_builder.sh --choose=[Function]"
+    echo ""
+    echo "Some functions on Origami Kernel Builder:"
+    echo "1. Build a whole Kernel"
+    echo "2. Regenerate defconfig"
+    echo "3. Open menuconfig"
+    echo "4. Clean"
+    echo ""
+    echo "Place this script inside the Kernel Tree."
 }
-trap exit_on_signal_INTR SIGINT INT TERM
 
-#### Main script ####
-execute_operation() {
+send_msg_telegram() {
     case "$1" in
-        1)
-            clear
-            echo "Building kernel..."
-            
-            if [ "$SEND_TO_TG" -eq 1 ]; then
-            curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-            -d chat_id="$chat_id" \
-            -d "disable_web_page_preview=true" \
-            -d "parse_mode=html" \
-            -d text="
-<b>~~~ ORIGAMI CI ~~~</b>
+    1) curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+                -d chat_id="$chat_id" \
+                -d "disable_web_page_preview=true" \
+                -d "parse_mode=html" \
+                -d text="<b>~~~ ORIGAMI CI ~~~</b>
 <b>Build Started on ${BUILD_HOST}</b>
 <b>Build status</b>: <code>${kver}</code>
 <b>Builder</b>: <code>${BUILDER}</code>
@@ -114,196 +107,182 @@ execute_operation() {
 <b>Date</b>: <code>$(date)</code>
 <b>Zip Name</b>: <code>${zipn}</code>
 <b>Defconfig</b>: <code>${DEFCONFIG}</code>
-<b>Compiler</b>: ${KBUILD_COMPILER_STRING}
-<b>Branch</b>: $(git rev-parse --abbrev-ref HEAD)
-<b>Last Commit</b>: $(git log --pretty=format:'"%h : %s"' -1)
-"
-        fi
+<b>Compiler</b>: <code>${KBUILD_COMPILER_STRING}</code>
+<b>Branch</b>: <code>$(git rev-parse --abbrev-ref HEAD)</code>
+<b>Last Commit</b>: <code>$(git log --format="%s" -n 1): $(git log --format="%h" -n 1)</code>" \
+                -o /dev/null
+        ;;
+    2) curl -s -F document=@./out/build.log "https://api.telegram.org/bot$token/sendDocument" \
+                -F chat_id="$chat_id" \
+                -F "disable_web_page_preview=true" \
+                -F "parse_mode=html" \
+                -F caption="Build failed after ${minutes} minutes and ${seconds} seconds." \
+                -o /dev/null
+        ;;
+    3) curl -s -F document=@./out/target/"${zipn}".zip "https://api.telegram.org/bot$token/sendDocument" \
+                -F chat_id="$chat_id" \
+                -F "disable_web_page_preview=true" \
+                -F "parse_mode=html" \
+                -F caption="Build took ${minutes} minutes and ${seconds} seconds.
+<b>SHA512</b>: <code>${checksum}</code>" \
+                -o /dev/null
 
-            rm ./out/arch/${ARCH}/boot/Image.gz
-            rm ./out/arch/${ARCH}/boot/Image.gz-dtb
-            
-            if [ ! -f "./arch/${ARCH}/configs/${DEFCONFIG}" ]; then
-               echo "${DEFCONFIG} not found, exited with error status 2."
-                if [ "$SEND_TO_TG" -eq 1 ]; then
-                   curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-                   -d chat_id="$chat_id" \
-                   -d "disable_web_page_preview=true" \
-                   -d "parse_mode=html" \
-                   -d text="<code>${DEFCONFIG}</code> not found, exited with error status 2."
-           fi
-        exit 2
-     fi
-            
-            export KBUILD_BUILD_USER=${BUILDER}
-            export KBUILD_BUILD_HOST=${BUILD_HOST}
-            export LOCALVERSION=${localversion}
-            
-            make O=out ARCH=${ARCH} ${DEFCONFIG}
-            
-            START=$(date +"%s")
+        curl -s -F document=@./out/build.log "https://api.telegram.org/bot$token/sendDocument" \
+                -F chat_id="$chat_id" \
+                -F "disable_web_page_preview=true" \
+                -F "parse_mode=html" \
+                -F caption="Build log" \
+                -o /dev/null
+        ;;
+    esac
+}
 
-if [ "$USE_LLVM" -eq 1 ]; then
-    PATH="${PWD}/clang/bin:${PATH}:${PWD}/arm-gcc/bin:${PATH}:${PWD}/aarch64-gcc/bin:${PATH}" \
+compile_kernel() {
+    rm ./out/arch/${ARCH}/boot/Image.gz-dtb 2>/dev/null
+
+    export KBUILD_BUILD_USER=${BUILDER}
+    export KBUILD_BUILD_HOST=${BUILD_HOST}
+    export LOCALVERSION=${localversion}
+
+    make O=out ARCH=${ARCH} ${DEFCONFIG}
+
+    START=$(date +"%s")
+
     make -j"$PROCS" O=out \
-    ARCH=${ARCH} \
-    LD="${LINKER}" \
-    AR=llvm-ar \
-    AS=llvm-as \
-    NM=llvm-nm \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip \
-    CC="clang" \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE="${PWD}/aarch64-gcc/bin/aarch64-linux-gnu-" \
-    CROSS_COMPILE_ARM32="${PWD}/arm-gcc/bin/arm-linux-gnueabihf-" \
-    CONFIG_NO_ERROR_ON_MISMATCH=y \
-    CONFIG_DEBUG_SECTION_MISMATCH=y \
-    V=0 2>&1 | tee out/build.log
-else
-    PATH="${PWD}/clang/bin:${PATH}:${PWD}/arm-gcc/bin:${PATH}:${PWD}/aarch64-gcc/bin:${PATH}" \
-    make -j"$PROCS" O=out \
-    ARCH=${ARCH} \
-    CC="clang" \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE="${PWD}/aarch64-gcc/bin/aarch64-linux-gnu-" \
-    CROSS_COMPILE_ARM32="${PWD}/arm-gcc/bin/arm-linux-gnueabihf-" \
-    CONFIG_NO_ERROR_ON_MISMATCH=y \
-    CONFIG_DEBUG_SECTION_MISMATCH=y \
-    V=0 2>&1 | tee out/build.log
-fi
+        ARCH=${ARCH} \
+        LD="${LINKER}" \
+        AR=llvm-ar \
+        AS=llvm-as \
+        NM=llvm-nm \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        CC="clang" \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${PWD}/aarch64-gcc/bin/aarch64-linux-gnu-" \
+        CROSS_COMPILE_ARM32="${PWD}/arm-gcc/bin/arm-linux-gnueabihf-" \
+        CONFIG_NO_ERROR_ON_MISMATCH=y \
+        CONFIG_DEBUG_SECTION_MISMATCH=y \
+        V=0 2>&1 | tee out/build.log
 
-END=$(date +"%s")
-DIFF=$((END - START))
-minutes=$((DIFF / 60))
-seconds=$((DIFF % 60))
+    END=$(date +"%s")
+    DIFF=$((END - START))
+    export minutes=$((DIFF / 60))
+    export seconds=$((DIFF % 60))
+}
 
+zip_kernel() {
+    # Move kernel image to anykernel zip
+    cp ./out/arch/${ARCH}/boot/Image.gz-dtb ./anykernel
 
-if [ ! -f "./out/arch/${ARCH}/boot/Image.gz-dtb" ]; then
-  echo "build failed after ${minutes} minute(s) and ${seconds} second(s)."
-  if [ "$SEND_TO_TG" -eq 1 ]; then
-                         curl -F document=@./out/build.log "https://api.telegram.org/bot$token/sendDocument" \
-                         -F chat_id="$chat_id" \
-                         -F "disable_web_page_preview=true" \
-                         -F "parse_mode=html" \
-                         -F caption="
-Build failed after ${minutes} minute(s) and ${seconds} second(s)."
-fi
-exit 1
-fi
+    # Zip the kernel
+    zip -r9 ./anykernel/"${zipn}".zip ./anykernel/* -x .git README.md *placeholder
 
-cp ./out/arch/${ARCH}/boot/Image.gz-dtb ./anykernel3
-cd anykernel3
-zip -r9 "${zipn}".zip * -x .git README.md *placeholder
-checksum=$(sha512sum "${zipn}".zip | cut -f1 -d ' ')
-echo "Build took ${minutes} minute(s) and ${seconds} second(s)."
-echo "Kernel zip: ${PWD}/../out/target/${zipn}.zip"
-echo "SHA512: ${checksum}"
-echo ""
+    # Generate checksum of kernel zip
+    export checksum=$(sha512sum ./anykernel/"${zipn}".zip | cut -f1 -d ' ')
 
-if [ "$SEND_TO_TG" -eq 1 ]; then
-    curl -F document=@"${zipn}".zip "https://api.telegram.org/bot$token/sendDocument" \
-                         -F chat_id="$chat_id" \
-                         -F "disable_web_page_preview=true" \
-                         -F "parse_mode=html" \
-                         -F caption="
-Build took ${minutes} minute(s) and ${seconds} second(s).
-<b>SHA512</b>: <code>${checksum}</code>"
-      curl -F document=@../out/build.log "https://api.telegram.org/bot$token/sendDocument" \
-                         -F chat_id="$chat_id" \
-                         -F "disable_web_page_preview=true" \
-                         -F "parse_mode=html" \
-                         -F caption="Build log"
-fi
-if [ ! -d "../out/target" ]; then
-mkdir ../out/target
-fi
-mv "${zipn}".zip ../out/target
-rm Image.gz-dtb
-cd ..
-echo "press enter to continue or type 0 for Quit"
-read -r a1
-if [ "$a1" == "0" ]; then
-    exit 0
-else
-    clear
-    bash origami_kernel_builder.sh
-fi
-;;
-        2)
-            echo "Generating DTBs..."
-            time make -j"$PROCS" O=out dtbs dtbo.img dtb.img
-            echo "done!"
-            echo "press enter to continue or type 0 for Quit"
-        read -r a1
-        if [ "$a1" == "0" ]; then
-            exit 0
-        else
-            clear
-            bash origami_kernel_builder.sh
+    # Move the kernel zip to ./out/target
+    if [ ! -d "./out/target" ]; then
+        mkdir ../out/target
+    fi
+    rm -f ./anykernel/Image.gz-dtb
+    mv ./anykernel/${zipn}.zip ./out/target
+}
+
+build_kernel() {
+    echo "================================="
+    echo "Build Started on ${BUILD_HOST}"
+    echo "Build status: ${kver}"
+    echo "Builder: ${BUILDER}"
+    echo "Device: ${DEVICE}"
+    echo "Kernel Version: $(make kernelversion 2>/dev/null)"
+    echo "Date: $(date)"
+    echo "Zip Name: ${zipn}"
+    echo "Defconfig: ${DEFCONFIG}"
+    echo "Compiler: ${KBUILD_COMPILER_STRING}"
+    echo "Branch: $(git rev-parse --abbrev-ref HEAD)"
+    echo "Last Commit: $(git log --format="%s" -n 1): $(git log --format="%h" -n 1)"
+    echo "================================="
+
+    if [ "$SEND_TO_TG" -eq 1 ]; then
+        send_msg_telegram 1
+    fi
+
+    compile_kernel
+
+    if [ ! -f "./out/arch/${ARCH}/boot/Image.gz-dtb" ]; then
+        if [ "$SEND_TO_TG" -eq 1 ]; then
+            send_msg_telegram 2
         fi
+        echo "================================="
+        echo -e "${RED}Build failed${NOCOLOR} after ${minutes} minutes and ${seconds} seconds"
+        echo "See build log for troubleshooting."
+        echo "================================="
+        exit 1
+    fi
+
+    zip_kernel
+
+    echo "================================="
+    echo "Build took ${minutes} minutes and ${seconds} seconds."
+    echo "SHA512: ${checksum}"
+    echo "================================="
+
+    if [ "$SEND_TO_TG" -eq 1 ]; then
+        send_msg_telegram 3
+    fi
+}
+
+ragen_defconfig() {
+clear
+make O=out ARCH=${ARCH} ${DEFCONFIG}
+cp -rf ./out/.config ./arch/${ARCH}/config/${DEFCONFIG}
+}
+
+open_menuconfig() {
+clear
+make O=out ARCH=${ARCH} ${DEFCONFIG}
+echo -e "Note: Make sure you save the config with name '.config'"
+echo -e "      else the defconfig will not saved automatically."
+local count=8
+while [ $count -gt 0 ]; do
+    echo -ne -e "${LIGHTCYAN}menuconfig will be opened in $count seconds... \r${NOCOLOR}"
+    sleep 1
+    ((count--))
+done
+make O=out menuconfig
+cp -rf ./out/.config ./arch/${ARCH}/config/${DEFCONFIG}
+}
+
+execute_operation() {
+
+   loop_helper() {
+      read -p "Press enter to continue or type 0 for Quit: " a1
+
+      if [[ "$a1" == "0" ]]; then
+          exit 0
+      else
+          clear
+          bash "$0"
+      fi
+   }
+
+   case "$1" in
+        1) build_kernel
+            loop_helper
             ;;
-        3)
-            clear
-            echo "Regenerating defconfig..."
-            make O=out ARCH=${ARCH} ${DEFCONFIG}
-            cp -rf "${PWD}"/out/.config "${PWD}"/arch/${ARCH}/configs/"$CONFIG"
-            echo "done!"
-            echo "press enter to continue or type 0 for Quit"
-        read -r a1
-        if [ "$a1" == "0" ]; then
-            exit 0
-        else
-            clear
-            bash origami_kernel_builder.sh
-        fi
-            ;;
-        4)
-            clear
-            make O=out ARCH=${ARCH} ${DEFCONFIG}
-            make O=out menuconfig
-            echo "Exited menuconfig."
-            echo "press enter to continue or type 0 for Quit"
-            read -r a1
-            if [ "$a1" == "0" ]; then
-                exit 0
-           else
-            clear
-            bash origami_kernel_builder.sh
-         fi
-            ;;
-        5)
-            clear
+        2) regen_defconfig
+            loop_helper
+             ;;
+        3) open_menuconfig
+             loop_helper
+             ;;
+        4) clear
             make clean && make mrproper
-            echo "done!"
-            echo "press enter to continue or type 0 for Quit"
-        read -r a1
-        if [ "$a1" == "0" ]; then
-            exit 0
-        else
-            clear
-            bash origami_kernel_builder.sh
-        fi
+            loop_helper
             ;;
-        6)
-            exit 0
-            ;;
-        7)
-            echo "Usage: bash origami_kernel_builder.sh --choose=[Function]"
-            echo ""
-            echo "Some functions on Origami Kernel Builder:"
-            echo "1. Build a whole Kernel"
-            echo "2. Build DTBs"
-            echo "3. Regenerate defconfig"
-            echo "4. Open menuconfig"
-            echo "5. Clean"
-            echo ""
-            echo "Place this script inside the Kernel Tree"
-            ;;
-        *)
-            echo "error: Not a valid choices"
-            exit 1
-            ;;
+        5) exit 0 ;;
+        6) help_msg ;;
+        *) echo -e "${RED}error:${NOCOLOR} Invalid selection." && exit 1 ;;
     esac
 }
 
@@ -312,12 +291,12 @@ if [ $# -eq 0 ]; then
     echo "What do you want to do today?"
     echo ""
     echo "1. Build a whole Kernel"
-    echo "2. Build DTBs"
-    echo "3. Regenerate defconfig"
-    echo "4. Open menuconfig"
-    echo "5. Clean"
-    echo "6. Quit"
-    read -r choice
+    echo "2. Regenerate defconfig"
+    echo "3. Open menuconfig"
+    echo "4. Clean"
+    echo "5. Quit"
+    echo ""
+    read -p "Choice the number: " choice
 else
     case "$1" in
         --choose=1)
@@ -332,18 +311,16 @@ else
         --choose=4)
             choice=4
             ;;
-        --choose=5)
-            choice=5
-            ;;
         --help)
-            choice=7
+            choice=6
             ;;
         *)
-            echo "error: Not a valid argument"
+            echo -e "${RED}error:${NOCOLOR} Not a valid argument"
             echo "Try 'bash origami_kernel_builder.sh --help' for more information."
             exit 1
             ;;
     esac
 fi
 
+# Main script logic
 execute_operation "$choice"
